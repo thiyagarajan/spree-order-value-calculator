f6f14ba7bb2a1eb9023056ca589c5f06af8a222d
thiyagarajan <thiyagarajannv@yahoo.com>
2014-04-07 22:15:14 +0530

    bug fix

--- lib/spree/calculator/shipping/order_value.rb
+++ lib/spree/calculator/shipping/order_value.rb
@@ -17,20 +17,24 @@ class Spree::Calculator::Shipping::OrderValue < Spree::ShippingCalculator
     order.line_items.each do |item|
       total_price += item.price * item.quantity
     end
+    costs = costs_string_to_hash
 
-    def costs_string_to_hash
-      costs_string = self.preferred_price_table
-      costs = {}
-      costs_string.split.each do |cost_string|
-        values = cost_string.strip.split(':')
-        costs[values[0].strip.to_f] = values[1].strip.to_f
-      end
-      costs
+    costs.each do |cost_range, ship|
+      shipping = ship if cost_range === total_price
     end
-
-
-    shipping = total_price
-
     return shipping
+  end
+
+
+  def costs_string_to_hash
+    costs_string = self.preferred_price_table
+    costs = {}
+    initial_range = 0
+    costs_string.split.each do |cost_string|
+      values = cost_string.strip.split(':')
+      costs[initial_range..values[0].strip.to_f] = values[1].strip.to_f
+      initial_range = values[0].strip.to_f+0.01
+    end
+    costs
   end
 end
